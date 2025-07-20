6fa7255d8098f7e5edcbca2ab5fbf78810d7e4eb
Skride <42325154+SkrideOne@users.noreply.github.com>
2025-07-20 16:42:54 +0400

    Update bpf_endian.h

--- test/bpf/bpf_endian.h
+++ test/bpf/bpf_endian.h
@@ -8,11 +8,11 @@
 #include <stdint.h>
 static inline uint16_t __bpf_constant_htons(uint16_t v)
 {
-	return (v >> 8) | (v << 8);
+    return (v >> 8) | (v << 8);
 }
 static inline uint32_t __bpf_constant_htonl(uint32_t v)
 {
-	return __builtin_bswap32(v);
+    return __builtin_bswap32(v);
 }
 #define bpf_htons(x) __bpf_constant_htons(x)
 #define bpf_htonl(x) __bpf_constant_htonl(x)
